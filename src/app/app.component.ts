1039af099254c9e42f49da47dace4dc011a77924
ElianMelo <elianmelomorais@gmail.com>
2021-06-04 00:52:36 -0300

    Consumindo Web API e modularizando o código em camadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photos = [
-    {
-      url: "https://upload.wikimedia.org/wikipedia/pt/e/ed/Shrek%28personagem%29.jpg",
-      description: "shrek"
-    },
-    {
-      url: "https://www.einerd.com.br/wp-content/uploads/2019/04/Shrek-Fiona-capa-890x466.jpg",
-      description: "fiona"
-    },
-    {
-      url: "https://i.pinimg.com/originals/91/f1/d2/91f1d28c883a0295c813943101be6e81.png",
-      description: "biscoito"
-    }
-  ]
+export class AppComponent implements OnInit {
+  photos: Photo[] = [];
+  
+  constructor(private photoService: PhotoService) {}
+  
+  ngOnInit(): void {
+    this.photoService
+      .listFromUser("flavio")
+      .subscribe(photos => this.photos = photos);
+  }
 }
