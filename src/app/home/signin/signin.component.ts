c3d4ea706fbffc5c2d39cd4814d44a10ae6f606f
ElianMelo <elianmelomorais@gmail.com>
2021-10-06 21:16:14 -0300

    Utilização de router navigate e ViewChild

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 import { AuthService } from "src/app/core/auth.service";
 
 @Component({
@@ -7,10 +8,12 @@ import { AuthService } from "src/app/core/auth.service";
 })
 export class SignInComponent implements OnInit {
     loginForm: FormGroup;
+    @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
     constructor(
         private formBuilder: FormBuilder,
-        private authService: AuthService) {
+        private authService: AuthService,
+        private router: Router) {
 
     }
 
@@ -28,10 +31,11 @@ export class SignInComponent implements OnInit {
         this.authService
             .authenticate(userName, password)
             .subscribe(
-                () => console.log('autenticado'),
+                () => this.router.navigate(['user', userName]),
                 err => {
                     console.log(err);
                     this.loginForm.reset();
+                    this.userNameInput.nativeElement.focus();
                     alert('Invalid user name or password');
                 }
             );
